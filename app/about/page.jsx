2731c7c308dde2d72fca5d13969ed30e80eedc91
AlexTamesB <jtamesb@vlacademy.com>
2025-05-11 14:57:05 -0600

    Agregando menu de portfoliio

---
+++ app/about/page.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function About() {
+return (
+    <div className=" max-w-lg mx-auto p-8">
+<h1 className="text-4xl font-extrabold mb-4">About</h1>
+<p className="text-sm text-gray-700 dark:text-gray-200 "> Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum dolorum laboriosam facere, aperiam dicta quaerat 
+    quibusdam tempora ipsum quasi! Cumque maxime iusto, 
+    architecto earum totam iste ipsam ad et! Architecto!
+    </p>
+
+    </div> 
+);
+
+}
